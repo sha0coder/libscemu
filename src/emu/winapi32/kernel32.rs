0ef95d7be15e27ca229cfb46bf0c494cde434ade
sha0coder <sha0c0d3r@gmail.com>
2022-10-19 12:28:03 +0200

    kernel32!multibytetowidechar implementation

--- src/emu/winapi32/kernel32.rs
+++ src/emu/winapi32/kernel32.rs
@@ -109,6 +109,7 @@ pub fn gateway(addr:u32, emu:&mut emu::Emu) {
         0x75e976e6 => FlsSetValue(emu),
         0x75e8bb08 => SetLastError(emu),
         0x75e8a611 => lstrlen(emu),
+        0x75e9452b => MultiByteToWideChar(emu),
 
         _ => panic!("calling unimplemented kernel32 API 0x{:x} {}", addr, guess_api_name(emu, addr)),
     }
@@ -1807,5 +1808,36 @@ fn lstrlen(emu:&mut emu::Emu) {
     println!("{}** {} kernel32!lstrlen '{}' ={} {}", emu.colors.light_red, emu.pos, s, len, emu.colors.nc);
 
     emu.regs.rax = len;
+}
+
+fn MultiByteToWideChar(emu:&mut emu::Emu) {
+    let codepage = emu.maps.read_dword(emu.regs.get_esp())
+        .expect("kernel32!MultiByteToWideChar cannot read codepage");
+    let flags = emu.maps.read_dword(emu.regs.get_esp() + 4)
+        .expect("kernel32!MultiByteToWideChar cannot read flags");
+    let utf8_ptr = emu.maps.read_dword(emu.regs.get_esp() + 8)
+        .expect("kernel32!MultiByteToWideChar cannot read utf8_ptr") as u64;
+    let cbMultiByte = emu.maps.read_dword(emu.regs.get_esp() + 12)
+        .expect("kernel32!MultiByteToWideChar cannot read cbMultiByte");
+    let wide_ptr = emu.maps.read_dword(emu.regs.get_esp() + 16)
+        .expect("kernel32!MultiByteToWideChar cannot read wide_ptr") as u64;
+    let cchWideChar = emu.maps.read_dword(emu.regs.get_esp() + 20)
+        .expect("kernel32!MultiByteToWideChar cannot read cchWideChar");
+
+    for _ in 0..6 {
+        emu.stack_pop32(false);
+    }
+
+    let utf8 = emu.maps.read_string(utf8_ptr);
+    let mut wide = String::new();
+    for c in utf8.chars() {
+        wide.push_str(&format!("{}",c));
+        wide.push_str("\x00");
+    }
+
+    println!("{}** {} kernel32!MultiByteToWideChar '{}' {}", emu.colors.light_red, emu.pos, utf8, emu.colors.nc);
+
+    emu.maps.write_string(wide_ptr, &wide);
+    emu.regs.rax = wide.len() as u64;
 }
 
