3dc22b817c708b93c835fba48ed398898ed9f003
sha0coder <sha0c0d3r@gmail.com>
2022-12-14 11:05:42 +0100

    minor change

--- src/emu.rs
+++ src/emu.rs
@@ -1506,10 +1506,10 @@ impl Emu {
 
         let map_name = self.filename_to_mapname(&self.filename);
         if name == "code" || addr < constants::LIBS_BARRIER || name.starts_with(&map_name) {
-            if self.cfg.verbose >= 1 && addr >= constants::LIBS_BARRIER {
+            /*if self.cfg.verbose >= 1 && addr >= constants::LIBS_BARRIER {
                 println!("/!\\ alert, jumping the barrier 0x{:x} name:{} map_name:{} filename:{}", 
                          addr, name, map_name, &self.filename);
-            }
+            }*/
             self.regs.set_eip(addr);
         } else {
             if self.cfg.verbose >= 1 {
