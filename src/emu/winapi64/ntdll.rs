1f69e0fb11865b33e6dfd099b2adfc07518f7920
sha0coder <sha0c0d3r@gmail.com>
2022-12-17 23:44:01 +0100

    changing winapi return values

--- src/emu/winapi64/ntdll.rs
+++ src/emu/winapi64/ntdll.rs
@@ -260,7 +260,7 @@ fn RtlQueueWorkItem(emu:&mut emu::Emu) {
         println!("api: {} ", name);
     }
 
-    emu.regs.rax = 1; //constants::STATUS_SUCCESS;
+    emu.regs.rax = constants::STATUS_SUCCESS;
 }
 
 fn NtWaitForSingleObject(emu:&mut emu::Emu) {
@@ -271,7 +271,7 @@ fn NtWaitForSingleObject(emu:&mut emu::Emu) {
     println!("{}** {} ntdll!NtWaitForSingleObject  hndl: 0x{:x} timeout: {} {}",
              emu.colors.light_red, emu.pos, handle, timeout, emu.colors.nc);
 
-    emu.regs.rax = 1; //constants::STATUS_SUCCESS;
+    emu.regs.rax = constants::STATUS_SUCCESS;
 }
 
 
