1bbecd194017c1ca9f7756d7645348f53da7de00
sha0coder <sha0@badchecksum.net>
2024-09-17 21:52:45 +0200

    fix accesing r13-r15 from python, and verifying it from a string

--- src/emu/regs64.rs
+++ src/emu/regs64.rs
@@ -2291,7 +2291,7 @@ impl Regs64 {
     pub fn is_reg(&self, reg: &str) -> bool {
         match reg {
             "rax" | "rbx" | "rcx" | "rdx" | "rsi" | "rdi" | "rbp" | "rsp" | "rip" | "r8" | "r9"
-            | "r10" | "r11" | "r12" | "eax" | "ebx" | "ecx" | "edx" | "esi" | "edi" | "esp"
+            | "r10" | "r11" | "r12" | "r13" | "r14" | "r15" | "eax" | "ebx" | "ecx" | "edx" | "esi" | "edi" | "esp"
             | "ebp" | "eip" | "r8d" | "r9d" | "r10d" | "r11d" | "r12d" | "r13d" | "r14d"
             | "r15d" | "ax" | "bx" | "cx" | "dx" | "bp" | "sp" | "r8w" | "r9w" | "r10w"
             | "r11w" | "r12w" | "r13w" | "r14w" | "r15w" | "si" | "di" | "al" | "ah" | "bl"
